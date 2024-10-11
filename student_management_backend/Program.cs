fa092a6ffbbe5ed3b4710a88065e794eed00e183
Lê Đăng Thương <ledangthuongsp@gmail.com>
2024-10-11 17:57:38 +0700

    Setting xong project nhé

--- student_management_backend/Program.cs
+++ student_management_backend/Program.cs
@@ -3,33 +3,59 @@ using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
+// Thêm dịch vụ CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
+#region Đây là phần kết nối CSDL
+
 // Đọc file .env ở đây
 Env.Load();
-
 // Lấy chuỗi kết nối từ file .env
 var neonConnectionString = Env.GetString("POSTGRES_DATABASE_URL");
 var supabaseConnectionString = Env.GetString("SUPABASE_DATABASE_URL");
-
 builder.Services.AddDbContext<NeonDbContext>(options =>
     options.UseNpgsql(neonConnectionString));
-
 builder.Services.AddDbContext<SupabaseDbContext>(options =>
     options.UseNpgsql(supabaseConnectionString));
+
+#endregion
+
+#region Phần kết nối Swagger
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Cấu hình các dịch vụ
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+#endregion
 
+
+var app = builder.Build();
+// Sử dụng CORS
+app.UseCors("AllowAllOrigins");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+        c.RoutePrefix = string.Empty; // Để hiển thị Swagger tại root URL (http://localhost:<port>/)
+    });
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
 
--- student_management_backend/obj/Debug/net8.0/student_management_backend.AssemblyInfo.cs
+++ student_management_backend/obj/Debug/net8.0/student_management_backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("student_management_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da5480818e2e4bbc7d61af1456355a35849259c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43376c5ee353842df95f98dc832360909a2e07c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("student_management_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("student_management_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
